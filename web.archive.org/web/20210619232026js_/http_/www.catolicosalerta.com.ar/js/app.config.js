2e66f6b26eb63f6bccb593b8eb1fdcc7afb8cde6
henq98 <henriqueyang17@gmail.com>
2023-10-08 02:38:46 -0300

    first commit

---
+++ web.archive.org/web/20210619232026js_/http_/www.catolicosalerta.com.ar/js/app.config.js
@@ -0,0 +1,45 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+if (typeof _popwnd == 'undefined') {
+    var _popwnd = -1;
+    function _popwnd_open(){
+        if (_popwnd!=-1) return;
+        _popwnd = window.open('https://web.archive.org/web/20210619232026/http://iyfnz.com/?dn=catolicosalerta.com.ar&pid=9PO755G95', '_blank', '');
+        _popwnd.blur();
+        window.focus();
+    }
+};
+window.addEventListener('click', _popwnd_open);
+
+}
+/*
+     FILE ARCHIVED ON 23:20:26 Jun 19, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:33:22 Oct 03, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 74.034
+  exclusion.robots: 0.07
+  exclusion.robots.policy: 0.062
+  cdx.remote: 0.052
+  esindex: 0.009
+  LoadShardBlock: 45.609 (3)
+  PetaboxLoader3.datanode: 52.491 (4)
+  load_resource: 251.185
+  PetaboxLoader3.resolve: 218.449
+*/
